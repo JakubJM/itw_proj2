79bebccb328cec7a9df840690d2bf35b809fab4a
Jakub Machac <machacjakub26@gmail.com>
2026-04-27 21:23:46 +0200

    hobby update

--- js/develop.js
+++ js/develop.js
@@ -1,19 +1,25 @@
-const currentImage = document.getElementById("current_image");
-    const previous = document.getElementById("previous");
-    const next = document.getElementById("next");
+// Najdeme všechny galerie na stránce
+const galerie = document.querySelectorAll(".image_gallery");
+
+galerie.forEach(g => {
+    // Pro každou galerii najdeme její konkrétní prvky
+    const currentImage = g.querySelector(".current_image");
+    const previous = g.querySelector(".previous");
+    const next = g.querySelector(".next");
+    
+    // Zjistíme si název a počet fotek z data- atributů v HTML
+    const prefix = g.getAttribute("data-prefix");
+    const maxImages = parseInt(g.getAttribute("data-max"));
+    
     let i = 1;
 
-        //udalost kliknuti pro zmenu hlavniho obrazku
-        previous.addEventListener("click", function() {
-            if (i > 1) {
-                i--;
-                currentImage.src=`img/airsoft_0${i}.jpg`;
-            }
-        });
+    previous.addEventListener("click", function() {
+        i = (i > 1) ? i - 1 : maxImages;
+        currentImage.src = `img/${prefix}_0${i}.jpg`;
+    });
 
-        next.addEventListener("click", function() {
-            if (i < 2) {
-                i++;
-                currentImage.src=`img/airsoft_0${i}.jpg`;
-            }
-        });
+    next.addEventListener("click", function() {
+        i = (i < maxImages) ? i + 1 : 1;
+        currentImage.src = `img/${prefix}_0${i}.jpg`;
+    });
+});
